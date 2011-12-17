97e5f247abd93539f9c820a7846843fef305700f
Sandro Padin <sandropadin@gmail.com>
2011-12-17 16:44:24 -0600

    Initial Commit

---
+++ iTalkie/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  iTalkie
+//
+//  Created by Sandro Padin on 12/17/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ iTalkie/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iTalkie
+//
+//  Created by Sandro Padin on 12/17/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
